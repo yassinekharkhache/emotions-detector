1cbd92582a1ed1db304003564b80a55b41fded23
ykharkha <judpak71@gmail.com>
2026-04-11 12:46:49 +0100

    test

--- scripts/predict.py
+++ scripts/predict.py
@@ -5,13 +5,13 @@ from tensorflow.keras.models import load_model
 model = load_model("../results/model/final_emotion_model.keras")
 
 df = pd.read_csv("../data/test.csv")
-
 X, y = [], []
 for _, row in df.iterrows():
-    pixels = np.array(row["pixels"].split(), dtype="float32").reshape(48, 48)
+    pixels = np.array(row["pixels "].split(), dtype="float32").reshape(48, 48)
     X.append(pixels)
     if "emotion" in df.columns:
         y.append(row["emotion"])
+
 
 X = np.array(X) / 255.0
 X = np.expand_dims(X, -1)
